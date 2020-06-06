3868c3a386749146cf455a48d0a1bb281496925a
Gabriel Castellano <gabrielcastellano25@gmail.com>
2020-06-05 23:11:11 -0300

    Inicio

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from menu import Menu
+from mapa_tela import Mapa_tela
+from mapa import Mapa
+
+
+class Gerenciador(ScreenManager):
+    pass
+
 
 class MainApp(MDApp):
-    pass
+    def build(self):
+        return Gerenciador()
 
 MainApp().run()
---
+++ mapa_tela.py
@@ -0,0 +1,4 @@
+from kivy.uix.screenmanager import Screen
+
+class Mapa_tela(Screen):
+    pass
