cceae0304e7cf2620d021be566719b1aa00b1649
gfcastellano <gabrielcastellano25@gmail.com>
2020-06-18 18:03:02 -0300

    urllib3

--- Telas/info_tela.py
+++ Telas/info_tela.py
@@ -7,7 +7,7 @@ import webbrowser
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.label import MDLabel
-from urllib import parse
+from urllib3 import parse
 
 
 class Info_tela(Screen):
