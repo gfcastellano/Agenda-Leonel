80ad826818559a609ab767ff0dacf420c4a26301
gfcastellano <gabrielcastellano25@gmail.com>
2020-07-25 00:04:00 -0300

    Tela visita

--- Telas/visita_tela.py
+++ Telas/visita_tela.py
@@ -1,6 +1,8 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
+from kivymd.uix.picker import MDDatePicker
+from datetime import date
 
 
 class Visita_tela(Screen):
@@ -12,3 +14,29 @@ class Visita_tela(Screen):
         
         self.dados_clientes = app.dados_clientes
         self.dados_visitas  = app.dados_visitas
+        data = date.today()
+        self.primeiro_dia = str(data.day) if len(str(data.day)) > 1 else '0'+str(data.day)
+        self.primeiro_mes = str(data.month) if len(str(data.month)) > 1 else '0'+str(data.month)
+        self.primeiro_ano = str(data.year)
+        self.ids.data.text = self.primeiro_dia + '/' + self.primeiro_mes + '/' + self.primeiro_ano
+
+
+    def abrir_popup_data(self):
+        primeiro_calendario = MDDatePicker(callback = self.marcar_data_visita)
+        primeiro_calendario.open()
+
+    def marcar_data_visita(self, data):
+        self.primeiro_dia = str(data.day) if len(str(data.day)) > 1 else '0'+str(data.day)
+        self.primeiro_mes = str(data.month) if len(str(data.month)) > 1 else '0'+str(data.month)
+        self.primeiro_ano = str(data.year)
+        self.ids.data.text = self.primeiro_dia + '/' + self.primeiro_mes + '/' + self.primeiro_ano
+
+    def abrir_popup_data_lembrete(self):
+        primeiro_calendario = MDDatePicker(callback = self.marcar_data_lembrete)
+        primeiro_calendario.open()
+
+    def marcar_data_lembrete(self, data):
+        self.primeiro_dia = str(data.day) if len(str(data.day)) > 1 else '0'+str(data.day)
+        self.primeiro_mes = str(data.month) if len(str(data.month)) > 1 else '0'+str(data.month)
+        self.primeiro_ano = str(data.year)
+        self.ids.data_lembrete.text = self.primeiro_dia + '/' + self.primeiro_mes + '/' + self.primeiro_ano
